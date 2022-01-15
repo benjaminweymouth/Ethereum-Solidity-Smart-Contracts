821ba6f7ea82269c0d6d29a209a0f5ed5d627843
"benjaminweymouth" <"benjamin.weymouth@gmail.com">
2022-01-15 11:16:07 -0500

    jan 15 balance function2

--- Jan15Class/01-Ins_Solidity_Tokens/Unsolved/ArcadeToken.sol
+++ Jan15Class/01-Ins_Solidity_Tokens/Unsolved/ArcadeToken.sol
@@ -11,7 +11,19 @@ contract ArcadeToken {
     //token must have an exchange rate 
     // 1 token costs 100 wei 
 
-    uint exchange_rate = 100; 
-    
+    uint8 exchange_rate = 100; 
+
+    // Mappings in Solidity are similar to the concept of hash map in Java or dictionary in C and Python
+    mapping (address => uint) balances; 
+
+    // function for balances
+
+    function balance () public view returns (uint) {
+
+        return balances[msg.sender];
+
+
+    } 
+
 
 }
