82c9d24cb55345e80b5899e1b7004a1b3ac530bd
"benjaminweymouth" <"benjamin.weymouth@gmail.com">
2022-01-15 19:33:19 -0500

    Jan 15 class commit

--- Jan15Class/01-Ins_Solidity_Tokens/Unsolved/ArcadeToken.sol
+++ Jan15Class/01-Ins_Solidity_Tokens/Unsolved/ArcadeToken.sol
@@ -1,9 +1,9 @@
 pragma solidity ^0.5.0;
 
-
-
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/math/SafeMath.sol";
 contract ArcadeToken {
 
+    using SafeMath for uint;
     address payable owner = msg.sender; 
     // here we name the token
     string public symbol = "ARCD";
@@ -22,8 +22,37 @@ contract ArcadeToken {
 
         return balances[msg.sender];
 
+    } 
+
+    function transfer(address recipient, uint value) public {
+
+        //balances is a data structure
+        balances[msg.sender] = balances[msg.sender].sub(value); 
+        balances[recipient] = balances[msg.sender].add(value); 
+
 
     } 
+
+      function purchase( ) public payable{
+
+        //balances is a data structure
+        uint amount = msg.value / exchange_rate; 
+        balances[msg.sender] = balances[msg.sender].add(amount);
+        owner.transfer(msg.value);
+
+
+    } 
+
+    //any contract that creates topkens should come with a  MINT function
+
+    function mint(address recipient, uint amount) public{
+
+        require(msg.sender == owner, "You are not the contract ownser so you can't mint tokens!");
+        balances[recipient] = balances[recipient].add(amount);
+    }
+
+//we want to avoid underflow attacks. use open zeppelin "SafeMath"
+
 
 
 }
--- Jan15Class/02-Stu_Arcade_Token/Unsolved/ArcadeToken.sol
+++ Jan15Class/02-Stu_Arcade_Token/Unsolved/ArcadeToken.sol
@@ -3,3 +3,11 @@ Arcade Token
 */
 
 // Construct your ArcadeToken contract below:
+pragma solidity ^0.5.0;
+
+
+
+contract ArcadeToken {
+
+    
+}
